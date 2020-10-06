2951f7b5708f797f54926f8839f825f8b28aaebb
viniciussk888 <vinicius.cross07@gmail.com>
2020-10-06 17:35:14 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic setup for react projects
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Basic setup</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Basic setup</h1>
+      </main>
+    </div>
+  )
+}
